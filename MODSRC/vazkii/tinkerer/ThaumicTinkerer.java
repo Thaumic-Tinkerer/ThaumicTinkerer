a303578f2dbe50aade87e98555ef71527af4c08f
Vazkii <vazkii@hotmail.com>
2013-04-24 21:58:20 +0100

    Intitial Code Commit, added Mod files, etc.

---
+++ MODSRC/vazkii/tinkerer/ThaumicTinkerer.java
@@ -0,0 +1,47 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ThaumicTinkerer Mod.
+ * 
+ * ThaumicTinkerer is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * ThaumicTinkerer is a Derivative Work on Thaumcraft 3.
+ * Thaumcraft 3 © Azanor 2012
+ * (http://www.minecraftforum.net/topic/1585216-)
+ * 
+ * File Created @ [24 Apr 2013, 19:28:32 (GMT)]
+ */
+package vazkii.tinkerer;
+
+import thaumcraft.common.CommonProxy;
+import thaumcraft.common.Thaumcraft;
+import vazkii.tinkerer.core.proxy.TTCommonProxy;
+import vazkii.tinkerer.lib.LibMisc;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.MOD_VERSION, dependencies = LibMisc.DEPENDENCIES)
+public class ThaumicTinkerer {
+
+	@Instance(LibMisc.MOD_ID)
+	public static ThaumicTinkerer modInstance;
+
+	@SidedProxy(clientSide = LibMisc.CLIENT_PROXY, serverSide = LibMisc.COMMON_PROXY)
+	public static TTCommonProxy proxy;
+	
+	public static CommonProxy tcProxy;
+	
+	@PreInit
+	public void onPreInit(FMLPreInitializationEvent event) {
+		modInstance = this;
+		tcProxy = Thaumcraft.proxy;
+		
+		System.out.println(tcProxy);
+		System.out.println(tcProxy.toString());
+	}
+	
+}
---
+++ MODSRC/vazkii/tinkerer/lib/LibMisc.java
@@ -0,0 +1,28 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ThaumicTinkerer Mod.
+ * 
+ * ThaumicTinkerer is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * ThaumicTinkerer is a Derivative Work on Thaumcraft 3.
+ * Thaumcraft 3 © Azanor 2012
+ * (http://www.minecraftforum.net/topic/1585216-)
+ * 
+ * File Created @ [24 Apr 2013, 20:38:31 (GMT)]
+ */
+package vazkii.tinkerer.lib;
+
+public class LibMisc {
+	
+	public static final String MOD_ID = "ThaumicTinkerer";
+	public static final String MOD_NAME = "Thaumic Tinkerer";
+	public static final String MOD_VERSION = "sqrt(-1)";
+	
+	public static final String DEPENDENCIES = "required-after:Thaumcraft";
+
+	public static final String COMMON_PROXY = "vazkii.tinkerer.core.proxy.TTCommonProxy";
+	public static final String CLIENT_PROXY = "vazkii.tinkerer.core.proxy.TTClientProxy";
+	
+}
