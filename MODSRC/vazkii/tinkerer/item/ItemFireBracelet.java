3293e9e3c70048b86a9d01d6ccc218f1d94477d5
Vazkii <vasco.men@hotmail.com>
2013-05-02 12:51:45 +0100

    Added Code for the Infernal Bracelet Effect.

--- MODSRC/vazkii/tinkerer/item/ItemFireBracelet.java
+++ MODSRC/vazkii/tinkerer/item/ItemFireBracelet.java
@@ -17,9 +17,12 @@ package vazkii.tinkerer.item;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
+import vazkii.tinkerer.ThaumicTinkerer;
 import vazkii.tinkerer.client.util.helper.IconHelper;
 import vazkii.tinkerer.lib.LibFeatures;
 import cpw.mods.fml.relauncher.Side;
@@ -36,13 +39,41 @@ public class ItemFireBracelet extends ItemMod {
 	}
 
 	@Override
-	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World,	EntityPlayer par3EntityPlayer) {
+	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
 		if(par1ItemStack.getItemDamage() != LibFeatures.FIRE_BRACELET_CHARGES) {
-			par1ItemStack.damageItem(1, par3EntityPlayer);
-			// TODO Complete
+			int id = par3World.getBlockId(par4, par5, par6);
+			int meta = par3World.getBlockMetadata(par4, par5, par6);
+			ItemStack stack = new ItemStack(id, 1, meta);
+			ItemStack result = FurnaceRecipes.smelting().getSmeltingResult(stack);
+			
+			boolean did = false;
+			
+			if(result != null && result.getItem() instanceof ItemBlock) {
+				par3World.setBlock(par4, par5, par6, result.itemID, result.getItemDamage(), 2);
+				par1ItemStack.damageItem(1, par2EntityPlayer);
+				par3World.playSoundAtEntity(par2EntityPlayer, "fire.ignite", 0.6F, 1F); 
+				par3World.playSoundAtEntity(par2EntityPlayer, "fire.fire", 1F, 1F);
+				if(par3World.isRemote)
+					par2EntityPlayer.swingItem();
+				for(int i = 0; i < 25; i++) {
+					double x = par4 + Math.random();
+					double y = par5 + Math.random();
+					double z = par6 + Math.random();
+					
+					ThaumicTinkerer.tcProxy.wispFX2(par3World, x, y, z, (float) Math.random() / 2F, 4, true, (float) -Math.random() / 10F);
+				}
+				
+				did = true;
+			}
+			
+			if(par1ItemStack.getItemDamage() == LibFeatures.FIRE_BRACELET_CHARGES)
+				par3World.playSoundAtEntity(par2EntityPlayer, "thaumcraft.brain", 1F, 1F);
+			
+			return did;
 		}
-		return super.onItemRightClick(par1ItemStack, par2World, par3EntityPlayer);
+		return false;
 	}
+	
 
 	@Override
 	@SideOnly(Side.CLIENT)
