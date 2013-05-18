678fcabcf2aa7a9c554827523bb8d813f2bcd245
Vazkii <vasco.men@hotmail.com>
2013-05-18 16:50:01 +0100

    Made the Wand of Uprising and Sword of the Condor researches hidden.

--- MODSRC/vazkii/tinkerer/research/ModResearchItems.java
+++ MODSRC/vazkii/tinkerer/research/ModResearchItems.java
@@ -83,10 +83,10 @@ public final class ModResearchItems {
 		sigilTeleport = new ResearchItem(LibItemNames.TELEPORTATION_SIGIL_R, tags, -2, 26, new ItemStack(ModItems.teleportSigil, 0, 1)).setParents(Config.researchTTOE).setHidden().registerResearchItem();
 
 		tags = new ObjectTags().add(EnumTag.MAGIC, 8).add(EnumTag.POWER, 8).add(EnumTag.MOTION, 12).add(EnumTag.FLIGHT, 14);
-		wandUprising = new ResearchItem(LibItemNames.WAND_UPRISING_R, tags, 2, 7, ModItems.wandUprising).setParents(Config.researchElementalSword).registerResearchItem();
+		wandUprising = new ResearchItem(LibItemNames.WAND_UPRISING_R, tags, 2, 7, ModItems.wandUprising).setParentsHidden(Config.researchElementalSword).setHidden().registerResearchItem();
 
 		tags = new ObjectTags().add(EnumTag.FLIGHT, 12).add(EnumTag.MOTION, 8).add(EnumTag.POWER, 14).add(EnumTag.MAGIC, 12);
-		swordCondor = new ResearchItem(LibItemNames.SWORD_CONDOR_R, tags, 4, 7, ModItems.swordCondor).setParents(wandUprising).registerResearchItem();
+		swordCondor = new ResearchItem(LibItemNames.SWORD_CONDOR_R, tags, 4, 7, ModItems.swordCondor).setParentsHidden(wandUprising).setHidden().registerResearchItem();
 
 		tags = new ObjectTags().add(EnumTag.DEATH, 20).add(EnumTag.POWER, 16).add(EnumTag.TRAP, 12);
 		deathRune = new ResearchItem(LibItemNames.DEATH_RUNE_R, tags, 5, 11, ModItems.deathRune).setParents(wardChest).setHidden().registerResearchItem();
