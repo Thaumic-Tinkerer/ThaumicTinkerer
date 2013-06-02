f27407de6accafc68c13fa29d0c3daafb40ab280
Vazkii <vasco.men@hotmail.com>
2013-06-02 19:34:53 +0100

    But before I get started on the void aggregator, made the flux collector drop it's items when broken!

--- MODSRC/vazkii/tinkerer/block/eldritch/BlockFluxCollector.java
+++ MODSRC/vazkii/tinkerer/block/eldritch/BlockFluxCollector.java
@@ -14,9 +14,14 @@
  */
 package vazkii.tinkerer.block.eldritch;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
@@ -31,10 +36,14 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockFluxCollector extends BlockModContainer {
 
+	Random random;
+	
 	public BlockFluxCollector(int par1) {
 		super(par1, Material.iron);
 		setHardness(Block.obsidian.blockHardness);
         setBlockBounds(0.0625F, 0F, 0.0625F, 0.9375F, 0.55F, 0.9375F);
+        
+        random = new Random();
 	}
 
 	@Override
@@ -47,6 +56,44 @@ public class BlockFluxCollector extends BlockModContainer {
 
 		return true;
 	}
+
+	@Override
+    public void breakBlock(World par1World, int par2, int par3, int par4, int par5, int par6) {
+        TileEntityFluxCollector collector = (TileEntityFluxCollector) par1World.getBlockTileEntity(par2, par3, par4);
+
+        if (collector != null) {
+            for (int j1 = 0; j1 < collector.getSizeInventory(); ++j1) {
+                ItemStack itemstack = collector.getStackInSlot(j1);
+
+                if (itemstack != null) {
+                    float f = random.nextFloat() * 0.8F + 0.1F;
+                    float f1 = random.nextFloat() * 0.8F + 0.1F;
+                    EntityItem entityitem;
+
+                    for (float f2 = random.nextFloat() * 0.8F + 0.1F; itemstack.stackSize > 0; par1World.spawnEntityInWorld(entityitem)) {
+                        int k1 = random.nextInt(21) + 10;
+
+                        if (k1 > itemstack.stackSize)
+                            k1 = itemstack.stackSize;
+
+                        itemstack.stackSize -= k1;
+                        entityitem = new EntityItem(par1World, par2 + f, par3 + f1, par4 + f2, new ItemStack(itemstack.itemID, k1, itemstack.getItemDamage()));
+                        float f3 = 0.05F;
+                        entityitem.motionX = (float)random.nextGaussian() * f3;
+                        entityitem.motionY = (float)random.nextGaussian() * f3 + 0.2F;
+                        entityitem.motionZ = (float)random.nextGaussian() * f3;
+
+                        if (itemstack.hasTagCompound())
+                            entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
+                    }
+                }
+            }
+
+            par1World.func_96440_m(par2, par3, par4, par5);
+        }
+
+        super.breakBlock(par1World, par2, par3, par4, par5, par6);
+    }
 
 	@Override
 	@SideOnly(Side.CLIENT)
