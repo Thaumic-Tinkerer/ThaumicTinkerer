34680ddb5bcf098cb58388f9ccb7dd91a249d5a3
Vazkii <vasco.men@hotmail.com>
2013-09-14 15:25:31 +0100

    Starting on the enchantment system...

---
+++ MODSRC/vazkii/tinkerer/common/enchantment/core/EnchantmentData.java
@@ -0,0 +1,38 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ThaumicTinkerer Mod.
+ * 
+ * ThaumicTinkerer is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * ThaumicTinkerer is a Derivative Work on Thaumcraft 4.
+ * Thaumcraft 4 (c) Azanor 2012
+ * (http://www.minecraftforum.net/topic/1585216-)
+ * 
+ * File Created @ [14 Sep 2013, 15:08:23 (GMT)]
+ */
+package vazkii.tinkerer.common.enchantment.core;
+
+import java.util.List;
+
+import net.minecraft.util.ResourceLocation;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+
+public class EnchantmentData extends AspectList {
+	
+	public final ResourceLocation texture;
+	public final boolean vanilla;
+
+	public EnchantmentData(String texture, boolean vanilla) {
+		this.texture = new ResourceLocation(texture);
+		this.vanilla = vanilla;
+	}
+
+	@Override
+	public EnchantmentData add(Aspect aspect, int count) {
+		return ((EnchantmentData) super.add(aspect, count));
+	}
+	
+}
