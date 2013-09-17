5c669efedbfc83bce326c6758cc8ac3b60ce8304
Vazkii <vasco.men@hotmail.com>
2013-09-17 21:09:22 +0100

    Register de_DE lang.

--- MODSRC/vazkii/tinkerer/client/lib/LibResources.java
+++ MODSRC/vazkii/tinkerer/client/lib/LibResources.java
@@ -84,7 +84,7 @@ public final class LibResources {
 	public static final String ENCHANT_QUICK_DRAW = PREFIX_ENCHANTS + "quickDraw.png";
 	
 	public static final String[] LANGS = {
-		"en_US"
+		"en_US", "de_DE"
 	};
 
 }
