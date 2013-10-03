6d3abad18587c5ef9267f2644393725aa8c1e39e
Vazkii <vasco.men@hotmail.com>
2013-10-03 19:45:14 +0100

    Clean index.php up...

--- web/index.php
+++ web/index.php
@@ -5,21 +5,23 @@
 	<a href="http://www.minecraftforum.net/topic/1813058-">Thaumic Tinkerer Thread</a><br>
 	<a href="http://vazkii.us">Back to vazkii.us</a>
 	<hr><br>
-</html>
 
-<?php
-print('<body link="#111111" vlink="#444466" alink="#000099">'); 
-
-$file_url = 'https://raw.github.com/Vazkii/ThaumicTinkerer/master/web/urls.txt';
-$file_contents = file_get_contents($file_url);
-$file_contents_array = array_reverse(explode(PHP_EOL, $file_contents));
-
-foreach($file_contents_array as $line) {
-	$line_props = explode('=', $line);
-	$version = $line_props[0];
-	$url = $line_props[1];
+	<body link="#111111" vlink="#444466" alink="#000099">
 	
-	print("<a href=\"$url\">ThaumicTinkerer <b>$version</b></a><br>");
-}
+	<?php
 
-?>
+	$file_url = 'https://raw.github.com/Vazkii/ThaumicTinkerer/master/web/urls.txt';
+	$file_contents = file_get_contents($file_url);
+	$file_contents_array = array_reverse(explode(PHP_EOL, $file_contents));
+
+	foreach($file_contents_array as $line) {
+		$line_props = explode('=', $line);
+		$version = $line_props[0];
+		$url = $line_props[1];
+		
+		print("<a href=\"$url\">ThaumicTinkerer <b>$version</b></a><br>");
+	}
+	?>
+	
+</body>
+</html>
