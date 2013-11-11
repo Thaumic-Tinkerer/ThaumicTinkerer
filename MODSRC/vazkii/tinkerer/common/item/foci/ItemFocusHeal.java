c713bb3e39b6b507075a172aefd859dc6c4a8e9f
TheWhiteWolves <JMSmith5@uclan.ac.uk>
2013-11-11 23:47:56 +0000

    Add in Mending Focus code
    No research or anything for it yet though

--- MODSRC/vazkii/tinkerer/common/item/foci/ItemFocusHeal.java
+++ MODSRC/vazkii/tinkerer/common/item/foci/ItemFocusHeal.java
@@ -14,16 +14,45 @@
  */
 package vazkii.tinkerer.common.item.foci;
 
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
+import thaumcraft.common.config.Config;
+import thaumcraft.common.items.wands.ItemWandCasting;
+import vazkii.tinkerer.common.ThaumicTinkerer;
 
 public class ItemFocusHeal extends ItemModFocus {
 
-	private static final AspectList visUsage = new AspectList().add(Aspect.EARTH, 10).add(Aspect.WATER, 10);
+	private static final AspectList visUsage = new AspectList().add(Aspect.EARTH, 50).add(Aspect.WATER, 50);
 	
 	public ItemFocusHeal(int par1) {
 		super(par1);
+	}
+	
+	@Override
+	public ItemStack onFocusRightClick(ItemStack itemstack, World world, EntityPlayer p, MovingObjectPosition movingobjectposition) {
+		ItemWandCasting wand = (ItemWandCasting)itemstack.getItem();
+
+		if(p.shouldHeal()) {
+			if (wand.consumeAllVis(itemstack, p, getVisCost(), true)) {
+				p.heal(1 + EnchantmentHelper.getEnchantmentLevel(Config.enchPotency.effectId, wand.getFocusItem(itemstack)));
+			
+				world.playSoundAtEntity(p, "thaumcraft:upgrade", 0.4F, 1F);
+				
+				for (int i = 0; i < 32; ++i) {
+		            ThaumicTinkerer.tcProxy.sparkle((float)p.posX + p.worldObj.rand.nextFloat() - 0.5F, (float)p.posY + p.worldObj.rand.nextFloat(), (float)p.posZ + p.worldObj.rand.nextFloat() - 0.5F, 3);
+		        }
+			}
+		}
+		
+		if(world.isRemote)
+			p.swingItem();
+
+		return itemstack;
 	}
 	
 	@Override
@@ -33,7 +62,7 @@ public class ItemFocusHeal extends ItemModFocus {
 	
 	@Override
 	public boolean isVisCostPerTick() {
-		return true;
+		return false;
 	}
 	
 	@Override
