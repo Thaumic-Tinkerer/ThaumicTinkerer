0dd059f0b8f2f12871752b5b8c937a8dfad9b689
Vasco Lavos <vazkii@hotmail.com>
2013-11-23 18:49:19 +0000

    Update workspace stuff and srgobf.bat for further automation of setup.

--- srgobf.bat
+++ srgobf.bat
@@ -1,2 +1,4 @@
-java -cp BON.jar immibis.bon.cui.MCPRemap -mcp E:/MCPDev/MCP -in TC.zip -out TCDEOBF.zip -side UNIVERSAL -from SRG -to OBF -ref E:/MCPDev/MCP/jars/bin/minecraft.jar
+java -cp BON.jar immibis.bon.cui.MCPRemap -mcp D:/MCPDev/MCP16/forge/mcp -in TC.zip -out TCDEOBF_IM.zip -side UNIVERSAL -from SRG -to OBF -ref D:/MCPDev/MCP16/forge/mcp/jars/versions/1.6.4/1.6.4.jar
+java -cp BON.jar immibis.bon.cui.MCPRemap -mcp D:/MCPDev/MCP16/forge/mcp -in TCDEOBF_IM.zip -out TCDEOBF.zip -side UNIVERSAL -from OBF -to MCP -ref D:/MCPDev/MCP16/forge/mcp/jars/versions/1.6.4/1.6.4.jar
+del TCDEOBF_IM.zip
 pause
