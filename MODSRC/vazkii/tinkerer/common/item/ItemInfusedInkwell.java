bf15eaea069df30e22ad213b8cbedf5d40eb898a
Vasco Lavos <vasco.men@hotmail.com>
2013-11-30 21:10:17 +0000

    Add hack for the research table. The item doesn't render, but that's with Azanor.

--- MODSRC/vazkii/tinkerer/common/item/ItemInfusedInkwell.java
+++ MODSRC/vazkii/tinkerer/common/item/ItemInfusedInkwell.java
@@ -1,8 +1,16 @@
 package vazkii.tinkerer.common.item;
 
+
+import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.player.PlayerOpenContainerEvent;
+import thaumcraft.common.container.ContainerResearchTable;
+import thaumcraft.common.container.SlotLimitedByClass;
 import thaumcraft.common.items.ItemInkwell;
 import vazkii.tinkerer.common.core.handler.ModCreativeTab;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -12,12 +20,23 @@ public class ItemInfusedInkwell extends ItemInkwell {
 		super(par1);
 		setCreativeTab(ModCreativeTab.INSTANCE);
 		setMaxDamage(800);
+		
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
 	@Override
 	@SideOnly(Side.CLIENT)
     public boolean hasEffect(ItemStack par1ItemStack, int pass) {
 		return true;
+	}
+	
+	@ForgeSubscribe
+	public void containerOpened(PlayerOpenContainerEvent event) {
+		Container container = event.entityPlayer.openContainer;
+		if(container instanceof ContainerResearchTable) {
+			SlotLimitedByClass slot = (SlotLimitedByClass) container.inventorySlots.get(2);
+			ReflectionHelper.setPrivateValue(SlotLimitedByClass.class, slot, ItemInfusedInkwell.class, 0);
+		}
 	}
 
 }
