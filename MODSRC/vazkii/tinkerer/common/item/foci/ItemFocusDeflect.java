aa9d3cb65c1ac82cb957f21b421c505ebf0bc0de
Vasco Lavos <vasco.men@hotmail.com>
2013-12-08 18:56:55 +0000

    Effects n stuff.

--- MODSRC/vazkii/tinkerer/common/item/foci/ItemFocusDeflect.java
+++ MODSRC/vazkii/tinkerer/common/item/foci/ItemFocusDeflect.java
@@ -14,23 +14,46 @@
  */
 package vazkii.tinkerer.common.item.foci;
 
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.IProjectile;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
+import thaumcraft.client.codechicken.core.vec.Vector3;
 import thaumcraft.common.config.Config;
+import thaumcraft.common.items.wands.ItemWandCasting;
+import vazkii.tinkerer.common.ThaumicTinkerer;
 
 public class ItemFocusDeflect extends ItemModFocus {
 
-	AspectList visUsage = new AspectList().add(Aspect.ORDER, 15).add(Aspect.AIR, 5);
+	AspectList visUsage = new AspectList().add(Aspect.ORDER, 8).add(Aspect.AIR, 4);
 	
 	public ItemFocusDeflect(int par1) {
 		super(par1);
 	}
 	
 	@Override
-	public void onUsingFocusTick(ItemStack paramItemStack, EntityPlayer paramEntityPlayer, int paramInt) {
-		// TODO
+	public void onUsingFocusTick(ItemStack stack, EntityPlayer p, int ticks) {
+		ItemWandCasting wand = (ItemWandCasting) stack.getItem();
+
+		if(wand.consumeAllVis(stack, p, getVisCost(), true)) {
+			List<Entity> projectiles = p.worldObj.getEntitiesWithinAABB(IProjectile.class, AxisAlignedBB.getBoundingBox(p.posX - 3, p.posY - 3, p.posZ - 3, p.posX + 2, p.posY + 2, p.posZ + 2));
+			
+			for(Entity e : projectiles) {
+				Vector3 motionVec = new Vector3(e.motionX, e.motionY, e.motionZ).normalize().multiply(Math.sqrt((e.posX - p.posX) * (e.posX - p.posX) + (e.posY - p.posY) * (e.posY - p.posY) + (e.posZ - p.posZ) * (e.posZ - p.posZ)) * 2) ;
+				
+				for(int i = 0; i < 6; i++)
+					ThaumicTinkerer.tcProxy.sparkle((float) e.posX, (float) e.posY, (float) e.posZ, 6);
+				
+				e.posX += motionVec.x;
+				e.posY += motionVec.y;
+				e.posZ += motionVec.z;				
+			}
+		}
 	}
 	
 	@Override
