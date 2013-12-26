4a50244f2427ddd1c608ba94a3737caa7e30e298
Vasco Lavos <vasco.men@hotmail.com>
2013-12-26 17:08:17 +0000

    Added wings to the chestpiece's model. Now I gotta animate them...

--- MODSRC/vazkii/tinkerer/common/item/ModItems.java
+++ MODSRC/vazkii/tinkerer/common/item/ModItems.java
@@ -26,6 +26,7 @@ import vazkii.tinkerer.common.item.foci.ItemFocusFlight;
 import vazkii.tinkerer.common.item.foci.ItemFocusHeal;
 import vazkii.tinkerer.common.item.foci.ItemFocusSmelt;
 import vazkii.tinkerer.common.item.foci.ItemFocusTelekinesis;
+import vazkii.tinkerer.common.item.kami.ItemGemChest;
 import vazkii.tinkerer.common.item.kami.ItemGemHelm;
 import vazkii.tinkerer.common.item.kami.ItemIchorclothArmor;
 import vazkii.tinkerer.common.item.kami.ItemIchorclothArmorAdv;
@@ -100,7 +101,7 @@ public final class ModItems {
 			ichorLegs = new ItemIchorclothArmor(LibItemIDs.idIchorLegs, 2).setUnlocalizedName(LibItemNames.ICHOR_LEGS);
 			ichorBoots = new ItemIchorclothArmor(LibItemIDs.idIchorBoots, 3).setUnlocalizedName(LibItemNames.ICHOR_BOOTS);
 			ichorHelmGem = new ItemGemHelm(LibItemIDs.idIchorHelmGem, 0).setUnlocalizedName(LibItemNames.ICHOR_HELM_GEM);
-			ichorChestGem = new ItemIchorclothArmorAdv(LibItemIDs.idIchorChestGem, 1).setUnlocalizedName(LibItemNames.ICHOR_CHEST_GEM);
+			ichorChestGem = new ItemGemChest(LibItemIDs.idIchorChestGem, 1).setUnlocalizedName(LibItemNames.ICHOR_CHEST_GEM);
 			ichorLegsGem = new ItemIchorclothArmorAdv(LibItemIDs.idIchorLegsGem, 2).setUnlocalizedName(LibItemNames.ICHOR_LEGS_GEM);
 			ichorBootsGem = new ItemIchorclothArmorAdv(LibItemIDs.idIchorBootsGem, 3).setUnlocalizedName(LibItemNames.ICHOR_BOOTS_GEM);
 			
