608992f3a94afea415226e95b7226fe4e3b2c2f8
Vasco Lavos <vasco.men@hotmail.com>
2013-12-27 20:31:47 +0000

    Added the effect of the Feline Amulet.

--- MODSRC/vazkii/tinkerer/common/item/kami/ItemCatAmulet.java
+++ MODSRC/vazkii/tinkerer/common/item/kami/ItemCatAmulet.java
@@ -14,12 +14,25 @@
  */
 package vazkii.tinkerer.common.item.kami;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAITaskEntry;
+import net.minecraft.entity.item.EntityEnderCrystal;
+import net.minecraft.entity.passive.EntityOcelot;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import vazkii.tinkerer.client.core.proxy.TTClientProxy;
 import vazkii.tinkerer.common.item.ItemMod;
+import vazkii.tinkerer.common.lib.LibObfuscation;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 
 public class ItemCatAmulet extends ItemMod {
 
@@ -29,8 +42,24 @@ public class ItemCatAmulet extends ItemMod {
 	}
 	
 	@Override
-	public void onUpdate(ItemStack par1ItemStack, World par2World, Entity par3Entity, int par4, boolean par5) {
-		// TODO
+	public void onUpdate(ItemStack par1ItemStack, World par2World, Entity e, int par4, boolean par5) {
+		int range = 10;
+		int rangeY = 4;
+		List<EntityLiving> entities = par2World.getEntitiesWithinAABB(EntityLiving.class, AxisAlignedBB.getBoundingBox(e.posX - range, e.posY - rangeY, e.posZ - range, e.posX + range, e.posY + rangeY, e.posZ + range));
+		
+		for(EntityLiving entity : entities) {
+            List<EntityAITaskEntry> entries = new ArrayList(entity.tasks.taskEntries);
+            entries.addAll(new ArrayList(entity.targetTasks.taskEntries));
+            
+            boolean avoidsOcelots = false;
+            for(EntityAITaskEntry entry : entries) {
+            	if(entry.action instanceof EntityAIAvoidEntity)
+            		avoidsOcelots = messWithRunAwaykAI((EntityAIAvoidEntity) entry.action) || avoidsOcelots;
+            	
+            	if(entry.action instanceof EntityAINearestAttackableTarget)
+            		messWithGetTargetAI((EntityAINearestAttackableTarget) entry.action);
+            }
+		}
 	}
 	
 	@Override
@@ -38,4 +67,16 @@ public class ItemCatAmulet extends ItemMod {
 		return TTClientProxy.kamiRarity;
 	}
 
+	private boolean messWithRunAwaykAI(EntityAIAvoidEntity aiEntry) {
+		if(ReflectionHelper.getPrivateValue(EntityAIAvoidEntity.class, aiEntry, LibObfuscation.TARGET_ENTITY_CLASS) == EntityOcelot.class) {
+			ReflectionHelper.setPrivateValue(EntityAIAvoidEntity.class, aiEntry, EntityPlayer.class, LibObfuscation.TARGET_ENTITY_CLASS);
+			return true;
+		}
+		return false;
+	}
+	
+	private void messWithGetTargetAI(EntityAINearestAttackableTarget aiEntry) {
+		if(ReflectionHelper.getPrivateValue(EntityAINearestAttackableTarget.class, aiEntry, LibObfuscation.TARGET_CLASS) == EntityPlayer.class)
+			ReflectionHelper.setPrivateValue(EntityAINearestAttackableTarget.class, aiEntry, EntityEnderCrystal.class, LibObfuscation.TARGET_CLASS); // Something random that won't be around
+	}
 }
