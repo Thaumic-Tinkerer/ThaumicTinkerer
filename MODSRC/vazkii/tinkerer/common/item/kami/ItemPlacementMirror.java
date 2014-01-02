bd425d7dd1e5b915742f0ef8202ad98da3e5a13b
Vasco Lavos <vasco.men@hotmail.com>
2014-01-02 15:27:40 +0000

    Fixed the Worldshaper's Looking Glass not having support for metadata.

--- MODSRC/vazkii/tinkerer/common/item/kami/ItemPlacementMirror.java
+++ MODSRC/vazkii/tinkerer/common/item/kami/ItemPlacementMirror.java
@@ -41,7 +41,7 @@ public class ItemPlacementMirror extends ItemMod {
 	@Override
 	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
 		int id = par3World.getBlockId(par4, par5, par6);
-		int meta = par3World.getBlockId(par5, par6, par7);
+		int meta = par3World.getBlockMetadata(par4, par5, par6);
 		
 		if(par2EntityPlayer.isSneaking()) {
 			if(Block.blocksList[id] != null && Block.blocksList[id].getRenderType() == 0)
