98454f40d2f723c0004225be905fbc778bd80913
Vasco Lavos <vasco.men@hotmail.com>
2014-01-10 21:59:15 +0000

    Moved dimension IDs to a config.

--- MODSRC/vazkii/tinkerer/common/core/handler/kami/DimensionalShardDropHandler.java
+++ MODSRC/vazkii/tinkerer/common/core/handler/kami/DimensionalShardDropHandler.java
@@ -21,6 +21,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import vazkii.tinkerer.common.core.handler.ConfigHandler;
 import vazkii.tinkerer.common.item.ModItems;
 
 public class DimensionalShardDropHandler {
@@ -28,10 +29,10 @@ public class DimensionalShardDropHandler {
 	@ForgeSubscribe
     public void onEntityLivingDrops(LivingDropsEvent event) {
         if(event.source.getEntity() != null && event.source.getEntity() instanceof EntityPlayer) {
-        	if(event.entityLiving instanceof EntityEnderman && event.entityLiving.dimension == 1 && Math.random() > 1D / 32D)
+        	if(event.entityLiving instanceof EntityEnderman && event.entityLiving.dimension == ConfigHandler.endDimensionID && Math.random() > 1D / 32D)
         		event.drops.add(new EntityItem(event.entityLiving.worldObj, event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ, new ItemStack(ModItems.kamiResource, 1, 7)));
 
-        	if(event.entityLiving instanceof EntityPigZombie && event.entityLiving.dimension == -1 && Math.random() > 1D / 16D)
+        	if(event.entityLiving instanceof EntityPigZombie && event.entityLiving.dimension == ConfigHandler.netherDimensionID && Math.random() > 1D / 16D)
         		event.drops.add(new EntityItem(event.entityLiving.worldObj, event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ, new ItemStack(ModItems.kamiResource, 1, 6)));
         }
     }
