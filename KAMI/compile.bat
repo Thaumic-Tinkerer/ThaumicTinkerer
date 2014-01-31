65e15f61d0277af63d059d07b95f0c695097e8e1
pixlepix <adlytempleton@gmail.com>
2014-01-30 19:33:20 -0500

    Moved things aroind for gradle

--- KAMI/compile.bat
+++
@@ -1,5 +0,0 @@
-mkdir ./vazkii/tinkerer/common/
-javac -source 1.6 -d . ThaumicTinkererKami.java
-jar cf ThaumicTinkererKAMI.jar ./vazkii/
-rd vazkii /s/q
-del .\cpw\mods\fml\common\Mod.class,.\cpw\mods\fml\common\network\NetworkMod.class
