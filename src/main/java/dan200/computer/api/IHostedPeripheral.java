1d2d236d3b034d77ef16a3f2403caece97e79fc5
nekosune <kat.swales@nekokittygames.com>
2014-01-31 23:39:53 +0000

    Added a dissapator turtle

--- src/main/java/dan200/computer/api/IHostedPeripheral.java
+++ src/main/java/dan200/computer/api/IHostedPeripheral.java
@@ -5,6 +5,7 @@
  */
 
 package dan200.computer.api;
+import dan200.computer.api.IPeripheral;
 
 /**
  * A subclass of IPeripheral specifically for peripherals
@@ -18,23 +19,23 @@ public interface IHostedPeripheral extends IPeripheral
 {
 	/**
 	 * A method called on each hosted peripheral once per tick, on the main thread
-	 * over the lifetime of the turtle or block. May be used to update the state
+	 * over the lifetime of the turtle or block. May be used to update the state 
 	 * of the peripheral, and may interact with IComputerAccess or ITurtleAccess
 	 * however it likes at this time.
 	 */
 	public void update();
-
+	
 	/**
 	 * A method called whenever data is read from the Turtle's NBTTag,
-	 * over the lifetime of the turtle. You should only use this for
+	 * over the lifetime of the turtle. You should only use this for 
 	 * reading data you want to stay with the peripheral.
 	 * @param nbttagcompound	The peripheral's NBTTag
 	 */
 	public void readFromNBT( net.minecraft.nbt.NBTTagCompound nbttagcompound );
-
+	
 	/**
 	 * A method called whenever data is written to the Turtle's NBTTag,
-	 * over the lifetime of the turtle. You should only use this for
+	 * over the lifetime of the turtle. You should only use this for 
 	 * writing data you want to stay with the peripheral.
 	 * @param nbttagcompound	The peripheral's NBTTag.
 	 * @param ID				The turtle's ID.
