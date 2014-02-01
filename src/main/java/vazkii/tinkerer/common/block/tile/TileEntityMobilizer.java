92d5ac53a66072b838ea0560a82b6f7a2c4fad4b
pixlepix <adlytempleton@gmail.com>
2014-01-31 22:09:43 -0500

    Basic infastructure for mobilizer.

--- src/main/java/vazkii/tinkerer/common/block/tile/TileEntityMobilizer.java
+++ src/main/java/vazkii/tinkerer/common/block/tile/TileEntityMobilizer.java
@@ -1,8 +1,10 @@
 package vazkii.tinkerer.common.block.tile;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
+import vazkii.tinkerer.common.lib.LibBlockIDs;
 
 public class TileEntityMobilizer extends TileEntity {
 
@@ -20,12 +22,40 @@ public class TileEntityMobilizer extends TileEntity {
 
 	public ForgeDirection movementDirection;
 
+	@Override
+	public void writeToNBT(NBTTagCompound nbt) {
+		super.writeToNBT(nbt);
+
+		nbt.setBoolean("Linked", linked);
+
+		nbt.setInteger("FirstRelayX", firstRelayX);
+		nbt.setInteger("FirstRelayZ", firstRelayZ);
+
+		nbt.setInteger("SecondRelayX", secondRelayX);
+		nbt.setInteger("SecondRelayZ", secondRelayZ);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+
+		this.linked = nbt.getBoolean("Linked");
+
+		this.firstRelayX = nbt.getInteger("FirstRelayX");
+		this.firstRelayZ = nbt.getInteger("FirstRelayZ");
+
+		this.secondRelayX = nbt.getInteger("SecondRelayX");
+		this.secondRelayZ = nbt.getInteger("SecondRelayZ");
+	}
+
+
+
 	public void verifyRelay(){
 		TileEntity te = worldObj.getBlockTileEntity(firstRelayX, yCoord, firstRelayZ);
 		if(te instanceof TileEntityRelay){
 			((TileEntityRelay) te).verifyPartner();
 		}
-		if(!(te instanceof TileEntityRelay && ((TileEntityRelay) te).partnerX == this.secondRelayX && ((TileEntityRelay) te).partnerZ == this.secondRelayZ)){
+		if(!(linked && te instanceof TileEntityRelay && ((TileEntityRelay) te).partnerX == this.secondRelayX && ((TileEntityRelay) te).partnerZ == this.secondRelayZ)){
 			linked=false;
 		}
 	}
@@ -33,19 +63,29 @@ public class TileEntityMobilizer extends TileEntity {
 	public void updateEntity(){
 		verifyRelay();
 
-		if(worldObj.getTotalWorldTime()%100==0){
+		if(linked && worldObj.getTotalWorldTime()%20==0){
 
-			//Switch direction if at end of track
-			if((xCoord+movementDirection.offsetZ==firstRelayX && zCoord + movementDirection.offsetZ == firstRelayZ) || (xCoord+movementDirection.offsetZ==secondRelayX && zCoord + movementDirection.offsetZ == secondRelayZ)){
-				movementDirection = movementDirection.getOpposite();
-			}
 
 			int targetX = xCoord+movementDirection.offsetX;
 			int targetZ = zCoord+movementDirection.offsetZ;
+			//Switch direction if at end of track
+			if(worldObj.getBlockId(targetX, yCoord, targetZ) != 0){
+				movementDirection = movementDirection.getOpposite();
+				targetX = xCoord+movementDirection.offsetX;
+				targetZ = zCoord+movementDirection.offsetZ;
+			}
+
 
 			if(worldObj.getBlockId(targetX, yCoord, targetZ) == 0){
-				worldObj.setBlock(targetX, yCoord, targetZ, this.blockType.blockID);
+				worldObj.setBlock(targetX, yCoord, targetZ, LibBlockIDs.idMobilizer);
+
+
+				worldObj.setBlockTileEntity(xCoord, yCoord, zCoord, this);
+
 				worldObj.setBlock(xCoord, yCoord, zCoord, 0);
+
+				this.xCoord = targetX;
+				this.zCoord = targetZ;
 			}
 
 
--- src/main/java/vazkii/tinkerer/common/block/tile/TileEntityRelay.java
+++ src/main/java/vazkii/tinkerer/common/block/tile/TileEntityRelay.java
@@ -2,14 +2,16 @@ package vazkii.tinkerer.common.block.tile;
 
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 
 public class TileEntityRelay extends TileEntity {
 	public TileEntityRelay(World world) {
 	}
 
+
 	public void verifyPartner(){
 		TileEntity te = worldObj.getBlockTileEntity(partnerX, yCoord, partnerZ);
-		if(!(te instanceof TileEntityRelay && ((TileEntityRelay) te).partnerX == this.xCoord && ((TileEntityRelay) te).partnerZ == this.zCoord)){
+		if(!(hasPartner && te instanceof TileEntityRelay && ((TileEntityRelay) te).partnerX == this.xCoord && ((TileEntityRelay) te).partnerZ == this.zCoord)){
 			hasPartner=false;
 		}
 	}
@@ -19,22 +21,81 @@ public class TileEntityRelay extends TileEntity {
 	public int partnerX;
 	public int partnerZ;
 
-	public void checkForPartner(){
-		for(int i=-32;i<32;i++){
-			TileEntity te = worldObj.getBlockTileEntity(xCoord+i, yCoord, partnerZ);
-			if(te instanceof TileEntityRelay){
-				((TileEntityRelay) te).partnerX=this.xCoord;
-				((TileEntityRelay) te).partnerZ=this.zCoord;
-				this.partnerX=te.xCoord;
-				this.partnerZ=te.zCoord;
-			}
+	@Override
+	public void updateEntity() {
 
-			te = worldObj.getBlockTileEntity(xCoord+i, yCoord, partnerZ);
-			if(te instanceof TileEntityRelay){
-				((TileEntityRelay) te).partnerX=this.xCoord;
-				((TileEntityRelay) te).partnerZ=this.zCoord;
-				this.partnerX=te.xCoord;
-				this.partnerZ=te.zCoord;
+		verifyPartner();
+
+		if(worldObj.getTotalWorldTime()%100==0){
+			checkForPartner();
+		}
+		int i=xCoord;
+		if(hasPartner && worldObj.getTotalWorldTime()%200==0){
+			do{
+
+				int j=zCoord;
+				do{
+					if(worldObj.getBlockTileEntity(i, yCoord, j) instanceof TileEntityMobilizer){
+						TileEntityMobilizer te = (TileEntityMobilizer) worldObj.getBlockTileEntity(i, yCoord, j);
+						te.verifyRelay();
+						if(!te.linked){
+							te.firstRelayX = xCoord;
+							te.firstRelayZ = zCoord;
+
+							te.secondRelayX = partnerX;
+							te.secondRelayZ = partnerZ;
+
+							te.linked = true;
+
+							if(xCoord < partnerX){
+								te.movementDirection = ForgeDirection.EAST;
+							}else if(xCoord > partnerX){
+								te.movementDirection = ForgeDirection.WEST;
+							}else if(zCoord < partnerZ){
+								te.movementDirection = ForgeDirection.SOUTH;
+							}else if(zCoord > partnerZ){
+								te.movementDirection = ForgeDirection.NORTH;
+							}
+						}
+					}
+
+					j+=Math.copySign(1,partnerZ-zCoord);
+				}while (j < partnerZ);
+				i+=Math.copySign(1, partnerX-xCoord);
+			}while (i < partnerX);
+		}
+	}
+
+	public void checkForPartner(){
+		if(!hasPartner){
+			for(int i=-10;i<10;i++){
+
+				TileEntity te = worldObj.getBlockTileEntity(xCoord+i, yCoord, zCoord);
+
+				if(te instanceof TileEntityRelay){
+					((TileEntityRelay) te).partnerX=this.xCoord;
+					((TileEntityRelay) te).partnerZ=this.zCoord;
+					this.partnerX=te.xCoord;
+					this.partnerZ=te.zCoord;
+
+					this.hasPartner=true;
+					((TileEntityRelay) te).hasPartner=true;
+
+				}
+
+
+
+				te = worldObj.getBlockTileEntity(xCoord+i, yCoord, zCoord);
+				if(te instanceof TileEntityRelay){
+					((TileEntityRelay) te).partnerX=this.xCoord;
+					((TileEntityRelay) te).partnerZ=this.zCoord;
+					this.partnerX=te.xCoord;
+					this.partnerZ=te.zCoord;
+
+
+					this.hasPartner=true;
+					((TileEntityRelay) te).hasPartner=true;
+				}
 			}
 		}
 	}
