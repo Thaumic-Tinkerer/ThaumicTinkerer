7e90e9c34b137b2ec76e7ae4399b0b52095d4c9c
nekosune <kat.swales@nekokittygames.com>
2014-02-05 15:02:57 +0000

    Changed url

--- src/main/java/vazkii/tinkerer/common/lib/LibMisc.java
+++ src/main/java/vazkii/tinkerer/common/lib/LibMisc.java
@@ -18,7 +18,7 @@ public final class LibMisc {
 
 	public static final String MOD_ID = "ThaumicTinkerer";
 	public static final String MOD_NAME = "Thaumic Tinkerer";
-	public static final String VERSION = "2.2b-75";
+	public static final String VERSION = "2.2b-76";
 
 	public static final String NETWORK_CHANNEL = MOD_ID;
 
