e6f5921958646db73a7ac95cba265a77d74ba9c5
nekosune <kat.swales@nekokittygames.com>
2014-02-06 02:55:18 +0000

    Added debug command to max out research

--- src/main/java/vazkii/tinkerer/common/lib/LibItemNames.java
+++ src/main/java/vazkii/tinkerer/common/lib/LibItemNames.java
@@ -77,4 +77,5 @@ public final class LibItemNames {
 		LibResources.PREFIX_MOD + "netherShard",
 		LibResources.PREFIX_MOD + "enderShard"
 	};
+	public static final String DEBUG_RESEARCH = LibResources.PREFIX_MOD + "debugResearch";;
 }
