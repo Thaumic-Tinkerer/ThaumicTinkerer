979937d6f2dd6e059e3a9821be88ffc6a398ba80
nekosune <kat.swales@nekokittygames.com>
2014-02-14 03:22:36 +0000

    Fixed research

--- src/main/java/vazkii/tinkerer/common/research/ModResearch.java
+++ src/main/java/vazkii/tinkerer/common/research/ModResearch.java
@@ -163,7 +163,7 @@ public final class ModResearch {
 
 			research = new TTResearchItem(LibResearch.KEY_ASPECT_ANALYZER, LibResearch.CATEGORY_THAUMICTINKERER, new AspectList().add(Aspect.MECHANISM, 2).add(Aspect.SENSES, 1).add(Aspect.MIND, 1), 2, 2, 2, new ItemStack(ModBlocks.aspectAnalyzer)).setParents(LibResearch.KEY_PERIPHERALS).setParentsHidden("GOGGLES", "THAUMIUM").setConcealed().setRound().registerResearchItem();
 			research.setPages(new ResearchPage("0"), arcaneRecipePage(LibResearch.KEY_ASPECT_ANALYZER));
-			research=new TTResearchItem(LibResearch.KEY_GOLEMCONNECTOR, LibResearch.CATEGORY_THAUMICTINKERER,new AspectList().add(Aspect.ORDER, 1).add(Aspect.TRAVEL, 2).add(Aspect.TOOL, 1),2,6,0,new ItemStack(ModBlocks.golemConnector)).setParents(LibResearch.KEY_PERIPHERALS).setConcealed().setRound().registerResearchItem();
+			research=new TTResearchItem(LibResearch.KEY_GOLEMCONNECTOR, LibResearch.CATEGORY_THAUMICTINKERER,new AspectList().add(Aspect.ORDER, 1).add(Aspect.TRAVEL, 2).add(Aspect.TOOL, 1),2,6,0,new ItemStack(ModBlocks.golemConnector)).setParents(LibResearch.KEY_PERIPHERALS).setParentsHidden("GOLEMBELL").setConcealed().setRound().registerResearchItem();
 			research.setPages(new ResearchPage("0"), arcaneRecipePage(LibResearch.KEY_GOLEMCONNECTOR), new ResearchPage("1"), arcaneRecipePage(LibResearch.KEY_CONNECTOR), new ResearchPage("2"));
 		}	
 
