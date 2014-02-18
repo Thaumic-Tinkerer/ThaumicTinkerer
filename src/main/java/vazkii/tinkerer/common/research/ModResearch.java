2c2ed48181e3df4742edabb1e6b4edcff832214b
pixlepix <adlytempleton@gmail.com>
2014-02-17 22:43:56 -0500

    Work on KAMI fix.

--- src/main/java/vazkii/tinkerer/common/research/ModResearch.java
+++ src/main/java/vazkii/tinkerer/common/research/ModResearch.java
@@ -171,7 +171,7 @@ public final class ModResearch {
 			research = new KamiResearchItem(LibResearch.KEY_DIMENSION_SHARDS, LibResearch.CATEGORY_THAUMICTINKERER, new AspectList(), 7, 8, 0, new ItemStack(ModItems.kamiResource, 1, 7)).setStub().setAutoUnlock().setRound().registerResearchItem();
 			research.setPages(new ResearchPage("0"));
 
-			research = new KamiResearchItem(LibResearch.KEY_ICHOR, LibResearch.CATEGORY_THAUMICTINKERER, new AspectList().add(Aspect.MAN, 1).add(Aspect.LIGHT, 2).add(Aspect.SOUL, 1).add(Aspect.TAINT, 1), 9, 8, 5, new ItemStack(ModItems.kamiResource, 1, 0)).setConcealed().setSpecial().registerResearchItem();
+			research = new KamiResearchItem(LibResearch.KEY_ICHOR, LibResearch.CATEGORY_THAUMICTINKERER, new AspectList().add(Aspect.MAN, 1).add(Aspect.LIGHT, 2).add(Aspect.SOUL, 1).add(Aspect.TAINT, 1), 9, 8, 5, new ItemStack(ModItems.kamiResource, 1, 0)).registerResearchItem();
 			research.setPages(new ResearchPage("0"), infusionPage(LibResearch.KEY_ICHOR));
 
 			research = new KamiResearchItem(LibResearch.KEY_ICHOR_CLOTH, LibResearch.CATEGORY_THAUMICTINKERER, new AspectList().add(Aspect.CLOTH, 2).add(Aspect.LIGHT, 1).add(Aspect.CRAFT, 1).add(Aspect.SENSES, 1), 11, 7, 5, new ItemStack(ModItems.kamiResource, 1, 1)).setConcealed().setParents(LibResearch.KEY_ICHOR).registerResearchItem();
