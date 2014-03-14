9a54a2a8f0ed91db29f5c168708ae395c5ac1656
pixlepix <adlytempleton@gmail.com>
2014-03-13 20:57:07 -0400

    Removed excess enchantments

--- src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentFiltration.java
+++
@@ -1,9 +0,0 @@
-package vazkii.tinkerer.common.enchantment;
-
-import net.minecraft.enchantment.EnumEnchantmentType;
-
-public class EnchantmentFiltration  extends EnchantmentMod {
-	public EnchantmentFiltration(int par1) {
-		super(par1, 5, EnumEnchantmentType.armor_head);
-	}
-}
--- src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentResolute.java
+++
@@ -1,9 +0,0 @@
-package vazkii.tinkerer.common.enchantment;
-
-import net.minecraft.enchantment.EnumEnchantmentType;
-
-public class EnchantmentResolute extends EnchantmentMod {
-	public EnchantmentResolute(int par1) {
-		super(par1, 5, EnumEnchantmentType.armor_torso);
-	}
-}
