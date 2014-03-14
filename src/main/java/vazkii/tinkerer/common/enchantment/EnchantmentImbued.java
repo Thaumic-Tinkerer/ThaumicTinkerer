755166661bbcfce40aa8c5aac59953f7aa81bbaf
pixlepix <adlytempleton@gmail.com>
2014-03-13 20:57:07 -0400

    Removed excess enchantments

--- src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentImbued.java
+++
@@ -1,9 +0,0 @@
-package vazkii.tinkerer.common.enchantment;
-
-import net.minecraft.enchantment.EnumEnchantmentType;
-
-public class EnchantmentImbued extends EnchantmentMod {
-	public EnchantmentImbued(int par1) {
-		super(par1, 5, EnumEnchantmentType.armor_head);
-	}
-}
