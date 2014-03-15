228161e3433e5bd3465a04f82ef9ca49218211db
pixlepix <adlytempleton@gmail.com>
2014-03-15 15:25:20 -0400

    Added enchantment icons

--- src/main/java/vazkii/tinkerer/client/lib/LibResources.java
+++ src/main/java/vazkii/tinkerer/client/lib/LibResources.java
@@ -99,13 +99,13 @@ public final class LibResources {
 		"en_US", "de_DE", "zh_CN", "ru_RU"
 	};
 
-	public static final String ENCHANT_FOCUSED_STRIKE = "";
-	public static final String ENCHANT_DISPERSED_STRIKE = "";
-	public static final String ENCHANT_VALIANCE = "";
-	public static final String ENCHANT_FINAL_STRIKE = "";
-	public static final String ENCHANT_TUNNEL="";
+	public static final String ENCHANT_FOCUSED_STRIKE = PREFIX_ENCHANTS + "focusedStrikes.png";
+	public static final String ENCHANT_DISPERSED_STRIKE = PREFIX_ENCHANTS + "dispersedStrikes.png";
+	public static final String ENCHANT_VALIANCE = PREFIX_ENCHANTS + "valiance.png";
+	public static final String ENCHANT_FINAL_STRIKE = PREFIX_ENCHANTS + "finalStrike.png";
+	public static final String ENCHANT_TUNNEL=PREFIX_ENCHANTS + "tunnel.png";
 
-	public static final String ENCHANT_SHATTER="";
-	public static final String ENCHANT_SHOCKWAVE="";
-	public static final String ENCHANT_POUNCE="";
+	public static final String ENCHANT_SHATTER=PREFIX_ENCHANTS + "shatter.png";
+	public static final String ENCHANT_SHOCKWAVE=PREFIX_ENCHANTS + "shockwave.png";
+	public static final String ENCHANT_POUNCE=PREFIX_ENCHANTS + "pounce.png";
 }
