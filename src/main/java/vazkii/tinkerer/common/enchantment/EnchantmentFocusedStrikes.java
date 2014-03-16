6d234ca83504f62cfbe130356afb4ee4dd2ccd54
pixlepix <adlytempleton@gmail.com>
2014-03-15 20:56:44 -0400

    Added compatibility rules

--- src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentFocusedStrikes.java
+++ src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentFocusedStrikes.java
@@ -1,10 +1,16 @@
 package vazkii.tinkerer.common.enchantment;
 
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
 
 public class EnchantmentFocusedStrikes extends EnchantmentMod {
 	public EnchantmentFocusedStrikes(int par1) {
 		super(par1, 5, EnumEnchantmentType.weapon);
+	}
+
+	@Override
+	public boolean canApplyTogether(Enchantment par1Enchantment) {
+		return par1Enchantment.effectId==Enchantment.knockback.effectId || par1Enchantment.effectId == Enchantment.looting.effectId;
 	}
 
 
