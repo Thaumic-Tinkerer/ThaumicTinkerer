f3b3033d74f1036e386d826d87e376683126d9c9
pixlepix <adlytempleton@gmail.com>
2014-03-15 20:56:44 -0400

    Added compatibility rules

--- src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentTunnel.java
+++ src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentTunnel.java
@@ -1,9 +1,15 @@
 package vazkii.tinkerer.common.enchantment;
 
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
 
 public class EnchantmentTunnel extends EnchantmentMod {
 	public EnchantmentTunnel(int par1) {
 		super(par1, 5, EnumEnchantmentType.digger);
+	}
+
+	@Override
+	public boolean canApplyTogether(Enchantment par1Enchantment) {
+		return super.canApplyTogether(par1Enchantment);
 	}
 }
