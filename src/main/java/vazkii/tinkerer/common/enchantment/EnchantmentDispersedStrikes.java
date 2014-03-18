d9365c55c76742c3d18daa87fc3056cf76db2171
pixlepix <adlytempleton@gmail.com>
2014-03-18 15:41:14 -0400

    Allowed enchantments to stack with unbreaking and repair

--- src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentDispersedStrikes.java
+++ src/main/java/vazkii/tinkerer/common/enchantment/EnchantmentDispersedStrikes.java
@@ -2,6 +2,7 @@ package vazkii.tinkerer.common.enchantment;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
+import thaumcraft.common.lib.enchantment.EnchantmentRepair;
 
 public class EnchantmentDispersedStrikes extends EnchantmentMod{
 	public EnchantmentDispersedStrikes(int par1) {
@@ -10,6 +11,6 @@ public class EnchantmentDispersedStrikes extends EnchantmentMod{
 
 	@Override
 	public boolean canApplyTogether(Enchantment par1Enchantment) {
-		return par1Enchantment.effectId==Enchantment.knockback.effectId || par1Enchantment.effectId == Enchantment.looting.effectId;
+		return  par1Enchantment.effectId == Enchantment.unbreaking.effectId || par1Enchantment instanceof EnchantmentRepair || par1Enchantment.effectId==Enchantment.knockback.effectId || par1Enchantment.effectId == Enchantment.looting.effectId;
 	}
 }
