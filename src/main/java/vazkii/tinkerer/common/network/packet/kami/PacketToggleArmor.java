065d4fef25ee850dde9050fba402a966e74fdad1
pixlepix <adlytempleton@gmail.com>
2014-06-06 19:36:46 -0400

    Fixed Keybinds not working. Bugs with packets still remain

--- src/main/java/vazkii/tinkerer/common/network/packet/kami/PacketToggleArmor.java
+++ src/main/java/vazkii/tinkerer/common/network/packet/kami/PacketToggleArmor.java
@@ -4,7 +4,6 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.entity.player.EntityPlayer;
 import vazkii.tinkerer.common.ThaumicTinkerer;
 import vazkii.tinkerer.common.network.packet.PacketTile;
