276f09c9ed1c5ddb88bb386e5aafab9bf91b9283
pixlepix <adlytempleton@gmail.com>
2014-06-21 15:21:28 -0400

    Fixed minor localization issues

--- src/main/java/vazkii/tinkerer/common/item/ItemInfusedPotion.java
+++ src/main/java/vazkii/tinkerer/common/item/ItemInfusedPotion.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class ItemInfusedPotion extends ItemPotion implements ITTinkererItem {
 
 	public String getItemStackDisplayName(ItemStack par1ItemStack) {
-		String s = "thaumictinkerer.item.infusedPotion." + getAspect(par1ItemStack).getName();
+		String s = "item.infusedPotion." + getAspect(par1ItemStack).getName() + ".name";
 		return StatCollector.translateToLocal(s).trim();
 	}
 
