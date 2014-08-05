f38fcb3235e5bd8e0e0308f3d2a69431d460e79e
pixlepix <adlytempleton@gmail.com>
2014-08-05 19:48:42 -0400

    Fixed forcefields dropping items when broken by water

--- src/main/java/thaumic/tinkerer/common/block/BlockForcefield.java
+++ src/main/java/thaumic/tinkerer/common/block/BlockForcefield.java
@@ -1,12 +1,15 @@
 package thaumic.tinkerer.common.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import thaumic.tinkerer.common.lib.LibBlockNames;
 import thaumic.tinkerer.common.potion.TileForcefield;
 import thaumic.tinkerer.common.registry.ThaumicTinkererRecipe;
 import thaumic.tinkerer.common.research.IRegisterableResearch;
+
+import java.util.ArrayList;
 
 /**
  * Created by pixlepix on 4/19/14.
@@ -21,8 +24,13 @@ public class BlockForcefield extends BlockMod {
 		return -1;
 	}
 
-	@Override
-	public boolean isOpaqueCube() {
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+        return new ArrayList<ItemStack>();
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
 		return false;
 	}
 
