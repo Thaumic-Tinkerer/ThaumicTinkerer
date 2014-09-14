1d1be5efd4fa72c9d68927ea30b231832ff41153
Pixlepix <adlytempleton@gmail.com>
2014-09-14 10:35:42 -0400

    Attempt at fixing 499

--- src/main/java/thaumic/tinkerer/common/potion/ModPotions.java
+++ src/main/java/thaumic/tinkerer/common/potion/ModPotions.java
@@ -43,17 +43,18 @@ public final class ModPotions {
 
 		for (Field f : Potion.class.getDeclaredFields()) {
 			f.setAccessible(true);
-
 			try {
 				if (f.getName().equals("potionTypes") || f.getName().equals("field_76425_a")) {
 					Field modfield = Field.class.getDeclaredField("modifiers");
 					modfield.setAccessible(true);
 					modfield.setInt(f, f.getModifiers() & ~Modifier.FINAL);
 					potionTypes = (Potion[]) f.get(null);
-					final Potion[] newPotionTypes = new Potion[256];
-					System.arraycopy(potionTypes, 0, newPotionTypes, 0, potionTypes.length);
-					f.set(null, newPotionTypes);
-				}
+                    if (f.getInt("length") < 256) {
+                        final Potion[] newPotionTypes = new Potion[256];
+                        System.arraycopy(potionTypes, 0, newPotionTypes, 0, potionTypes.length);
+                        f.set(null, newPotionTypes);
+                    }
+                }
 			} catch (Exception e) {
                 ThaumicTinkerer.log.error("Severe error, please report this to the mod author:",e);
 			}
