a4c0f34738a45ffe2e24a2f88231b6f788107b82
Katrina Swales <kat.swales@nekokittygames.com>
2015-01-31 14:26:38 +0100

    Revert "Kill language handler"

---
+++ src/main/java/thaumic/tinkerer/client/core/handler/LocalizationHandler.java
@@ -0,0 +1,27 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ThaumicTinkerer Mod.
+ *
+ * ThaumicTinkerer is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ *
+ * ThaumicTinkerer is a Derivative Work on Thaumcraft 4.
+ * Thaumcraft 4 (c) Azanor 2012
+ * (http://www.minecraftforum.net/topic/1585216-)
+ *
+ * File Created @ [4 Sep 2013, 17:03:43 (GMT)]
+ */
+package thaumic.tinkerer.client.core.handler;
+
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import thaumic.tinkerer.client.lib.LibResources;
+
+public final class LocalizationHandler {
+
+    public static void loadLocalizations() {
+        for (String locale : LibResources.LANGS)
+            LanguageRegistry.instance().loadLocalization(LibResources.PREFIX_LANG + locale + ".lang", locale, false);
+    }
+
+}
