39eab65c6c9c1329a28ac36269a05fa0ea0c70a3
Katrina Swales <kat.swales@nekokittygames.com>
2015-07-06 20:29:22 +0100

    Added more lore and other stuff

--- src/main/scala/com/nekokittygames/Thaumic/Tinkerer/common/items/baubles/ItemEnderDisruption.scala
+++ src/main/scala/com/nekokittygames/Thaumic/Tinkerer/common/items/baubles/ItemEnderDisruption.scala
@@ -1,21 +1,22 @@
 package com.nekokittygames.Thaumic.Tinkerer.common.items.baubles
 
-import baubles.api.{BaublesApi, BaubleType}
+import baubles.api.{BaubleType, BaublesApi}
 import com.nekokittygames.Thaumic.Tinkerer.common.libs.LibItemNames
-import net.minecraft.entity.{Entity, EntityLivingBase}
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
-import net.minecraft.util.AxisAlignedBB
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.entity.living.EnderTeleportEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
 import scala.collection.JavaConversions._
 /**
  * Created by Katrina on 07/06/2015.
  */
 object ItemEnderDisruption extends ItemBaubles(BaubleType.AMULET){
   setUnlocalizedName(LibItemNames.ENDERDISRUPTION)
+  setMaxDamage(600)
 
 
   override def initItem(fMLPreInitializationEvent: FMLPreInitializationEvent): Unit =
@@ -48,8 +49,10 @@ object ItemEnderDisruption extends ItemBaubles(BaubleType.AMULET){
         for( i <- 0 to inventory.getSizeInventory)
           {
             val itemStack=inventory.getStackInSlot(i)
-            if(itemStack!=null && itemStack.getItem==ItemEnderDisruption)
+            if(itemStack!=null && itemStack.getItem==ItemEnderDisruption && itemStack.getItemDamage<itemStack.getMaxDamage) {
               event.setCanceled(true)
+              itemStack.setItemDamage(Math.min(itemStack.getMaxDamage,itemStack.getItemDamage+1))
+            }
           }
       }
   }
