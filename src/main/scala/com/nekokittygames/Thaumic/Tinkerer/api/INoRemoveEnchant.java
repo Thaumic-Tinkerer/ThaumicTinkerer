b590ff5b6c2dcb2271ed9265489ccb654c339bc9
Katrina Swales <kat.swales@nekokittygames.com>
2018-02-17 15:01:20 +0000

    Started TT for 1.10

--- src/main/scala/com/nekokittygames/Thaumic/Tinkerer/api/INoRemoveEnchant.java
+++
@@ -1,7 +0,0 @@
-package com.nekokittygames.Thaumic.Tinkerer.api;
-
-/**
- * Created by Katrina on 17/05/2015.
- */
-public interface INoRemoveEnchant {
-}
--- src/main/scala/com/nekokittygames/Thaumic/Tinkerer/common/libs/LibFeatures.scala
+++
@@ -1,9 +0,0 @@
-package com.nekokittygames.Thaumic.Tinkerer.common.libs
-
-/**
- * Created by katsw on 30/10/2015.
- */
-object LibFeatures {
-  final var CLEANSING_TALISMAN_USES=100
-
-}
