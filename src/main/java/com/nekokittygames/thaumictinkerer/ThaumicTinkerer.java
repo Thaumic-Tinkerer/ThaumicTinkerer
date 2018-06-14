67f57b090677801f6230849a37c90fd5c0b0cc3e
Katrina Swales <kat.swales@nekokittygames.com>
2018-06-14 02:48:30 +0100

    Starting again :(

---
+++ src/main/java/com/nekokittygames/thaumictinkerer/ThaumicTinkerer.java
@@ -0,0 +1,32 @@
+package com.nekokittygames.thaumictinkerer;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = com.example.examplemod.ExampleMod.MODID, name = com.example.examplemod.ExampleMod.NAME, version = com.example.examplemod.ExampleMod.VERSION)
+public class ThaumicTinkerer
+{
+    public static final String MODID = "examplemod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
+
