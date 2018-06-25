3486a243502fc645563ae8694c0dd570262eb4e5
Katrina Swales <kat.swales@nekokittygames.com>
2018-06-25 17:53:04 +0100

    Added GUI location (Testing Webhooks)

--- src/main/java/com/nekokittygames/thaumictinkerer/client/gui/GuiMobMagnet.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/client/gui/GuiMobMagnet.java
@@ -1,6 +1,8 @@
 package com.nekokittygames.thaumictinkerer.client.gui;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ResourceLocation;
 
 public class GuiMobMagnet extends GuiScreen {
+    public static final ResourceLocation GUI_BACKGROUND=new ResourceLocation("thaumictinkerer","textures/gui/mob_magnet.png");
 }
