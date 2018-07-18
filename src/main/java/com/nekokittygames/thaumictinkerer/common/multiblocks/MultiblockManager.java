218a8bf52077d3e63b404fb3a18df772736dce66
Katrina Swales <kat.swales@nekokittygames.com>
2018-07-18 03:46:52 +0100

    Starting on Multiblock manager

---
+++ src/main/java/com/nekokittygames/thaumictinkerer/common/multiblocks/MultiblockManager.java
@@ -0,0 +1,12 @@
+package com.nekokittygames.thaumictinkerer.common.multiblocks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiblockManager {
+    private static List<Multiblock> multiblocks=new ArrayList<>();
+    public static void initMultiblocks()
+    {
+
+    }
+}
