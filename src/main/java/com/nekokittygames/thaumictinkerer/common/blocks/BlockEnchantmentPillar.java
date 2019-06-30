7eac241a50d5f55dbcc1b6e4762177cc8fa7787b
Katrina Swales <kat.swales@nekokittygames.com>
2019-06-30 19:42:22 +0100

    Fixed #973 , Pillars drop arcane stone and break the whole structure on break

--- src/main/java/com/nekokittygames/thaumictinkerer/common/blocks/BlockEnchantmentPillar.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/common/blocks/BlockEnchantmentPillar.java
@@ -1,6 +1,8 @@
 package com.nekokittygames.thaumictinkerer.common.blocks;
 
+import com.nekokittygames.thaumictinkerer.ThaumicTinkerer;
 import com.nekokittygames.thaumictinkerer.common.libs.LibBlockNames;
+import com.nekokittygames.thaumictinkerer.common.tileentity.TileEntityEnchanter;
 import com.nekokittygames.thaumictinkerer.common.tileentity.TileEntityEnchantmentPillar;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
@@ -9,14 +11,21 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import thaumcraft.api.blocks.BlocksTC;
+
+import java.util.Random;
 
 public class BlockEnchantmentPillar extends TTTileEntity<TileEntityEnchantmentPillar> {
     public static final PropertyBool Top = PropertyBool.create("top");
@@ -76,5 +85,49 @@ public class BlockEnchantmentPillar extends TTTileEntity<TileEntityEnchantmentPi
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         player.sendStatusMessage(new TextComponentString("Direction - " + state.getValue(Direction)), true);
         return true;
+    }
+
+    private BlockPos IsEnchanterPos(World world,BlockPos pos)
+    {
+        if(world.getBlockState(pos).getBlock()==ModBlocks.osmotic_enchanter)
+            return pos;
+        if(world.getBlockState(pos.down(1)).getBlock()==ModBlocks.osmotic_enchanter)
+            return pos.down(1);
+        return null;
+    }
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+        BlockPos enchanterLoc=null;
+        enchanterLoc=IsEnchanterPos(worldIn,pos.south(3));
+        if(enchanterLoc==null)
+            enchanterLoc = IsEnchanterPos(worldIn,pos.west(3));
+        if(enchanterLoc==null)
+            enchanterLoc = IsEnchanterPos(worldIn,pos.north(3));
+        if(enchanterLoc==null)
+            enchanterLoc = IsEnchanterPos(worldIn,pos.east(3));
+        if(enchanterLoc==null)
+            enchanterLoc = IsEnchanterPos(worldIn, pos.south(2).west(2));
+        if(enchanterLoc==null)
+            enchanterLoc = IsEnchanterPos(worldIn, pos.north(2).west(2));
+        if(enchanterLoc==null)
+            enchanterLoc = IsEnchanterPos(worldIn, pos.south(2).east(2));
+        if(enchanterLoc==null)
+            enchanterLoc = IsEnchanterPos(worldIn, pos.north(2).east(2));
+
+        ThaumicTinkerer.logger.info("Enchanter Pos Found - "+ (enchanterLoc != null ? enchanterLoc.toString() : "null"));
+        if(enchanterLoc!=null)
+        {
+            TileEntity te=worldIn.getTileEntity(enchanterLoc);
+            if(te instanceof TileEntityEnchanter)
+            {
+                ((TileEntityEnchanter)te).BreakPillars();
+            }
+        }
+    }
+
+    @NotNull
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Item.getItemFromBlock(BlocksTC.stoneArcane);
     }
 }
