90b099fd0081d2ddc5c35f0560747269bdaede34
Katrina Swales <kat.swales@nekokittygames.com>
2019-09-16 03:17:50 +0100

    Work towards adding a fancier block connection indicator

--- src/main/java/com/nekokittygames/thaumictinkerer/client/misc/RenderEvents.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/client/misc/RenderEvents.java
@@ -1,6 +1,23 @@
 package com.nekokittygames.thaumictinkerer.client.misc;
 
+import com.nekokittygames.thaumictinkerer.common.libs.LibMisc;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(modid = LibMisc.MOD_ID,value = Side.CLIENT)
 public class RenderEvents {
+
+    public static TextureAtlasSprite MARK_SPRITE;
+    @SubscribeEvent
+    public static void AddTextures(final TextureStitchEvent.Pre event) {
+        MARK_SPRITE=event.getMap().registerSprite(new ResourceLocation("thaumictinkerer:textures/misc/mark.png"));
+    }
  /*   private static StructureRendererHandler structureHandler = new StructureRendererHandler();
 
     @SideOnly(Side.CLIENT)
@@ -13,5 +30,8 @@ public class RenderEvents {
         }
 
     }
+
 */
+
+
 }
--- src/main/java/com/nekokittygames/thaumictinkerer/client/rendering/special/TransvectorRendering.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/client/rendering/special/TransvectorRendering.java
@@ -1,5 +1,6 @@
 package com.nekokittygames.thaumictinkerer.client.rendering.special;
 
+import com.nekokittygames.thaumictinkerer.client.misc.RenderEvents;
 import com.nekokittygames.thaumictinkerer.common.items.ItemConnector;
 import com.nekokittygames.thaumictinkerer.common.libs.LibMisc;
 import com.nekokittygames.thaumictinkerer.common.tileentity.transvector.TileEntityTransvector;
@@ -8,10 +9,12 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -53,11 +56,11 @@ public class TransvectorRendering {
             net.minecraft.client.renderer.RenderHelper.disableStandardItemLighting();
             Minecraft.getMinecraft().entityRenderer.disableLightmap();
             GlStateManager.disableTexture2D();
-            GlStateManager.disableBlend();
-            GlStateManager.disableLighting();
-            GlStateManager.disableAlpha();
+            //GlStateManager.disableBlend();
+            //GlStateManager.disableLighting();
+            //GlStateManager.disableAlpha();
             GlStateManager.glLineWidth(4);
-            GlStateManager.color(1, 1, 1);
+            //GlStateManager.color(1, 1, 1);
 
             Tessellator tessellator = Tessellator.getInstance();
             BufferBuilder buffer = tessellator.getBuffer();
@@ -67,9 +70,10 @@ public class TransvectorRendering {
             float r = 1f;
             float g = 0.2f;
             float b = 0.3f;
-            float a = 0.8f;
-            buffer.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
-            drawOutline(buffer, blockX, blockY, blockZ, r, g, b, a);
+            float a = 0.3f;
+            buffer.begin(GL11.GL_TRIANGLES, DefaultVertexFormats.POSITION_TEX);
+            drawTexturedOutline(buffer, blockX, blockY, blockZ, r, g, b, a,RenderEvents.MARK_SPRITE);
+
             TileEntity te = Minecraft.getMinecraft().world.getTileEntity(pos);
             if (te instanceof TileEntityTransvector) {
                 TileEntityTransvector transvector = (TileEntityTransvector) te;
@@ -81,16 +85,21 @@ public class TransvectorRendering {
                     r = 0.2f;
                     g = 1f;
                     b = 0.3f;
-                    a = 0.8f;
+                    a = 0.3f;
 
-                    drawOutline(buffer, blockX, blockY, blockZ, r, g, b, a);
-                    drawLine(buffer,pos.getX(),pos.getY(),pos.getZ(),blockX,blockY,blockZ,1f,0.2f,0.3f,0.8f,r,g,b,a);
+                    drawTexturedOutline(buffer, blockX, blockY, blockZ, r, g, b, a, RenderEvents.MARK_SPRITE);
+                    tessellator.draw();
+                    buffer = tessellator.getBuffer();
+
+                    buffer.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
+
+                    drawLine(buffer,pos.getX(),pos.getY(),pos.getZ(),blockX,blockY,blockZ,1f,0.2f,0.3f,0.3f,r,g,b,a);
                 }
             }
             tessellator.draw();
 
             Minecraft.getMinecraft().entityRenderer.enableLightmap();
-            GlStateManager.enableTexture2D();
+           GlStateManager.enableTexture2D();
             GlStateManager.enableDepth();
 
             GlStateManager.popMatrix();
@@ -103,6 +112,32 @@ public class TransvectorRendering {
     private static void drawLine(BufferBuilder buffer, int originX, int originY, int originZ, float blockX, float blockY, float blockZ, float r, float g, float b, float a,float gr, float gg, float gb, float ga) {
         buffer.pos(originX+0.5f,originY+0.5f,originZ+0.5f).color(r,g,b,a).endVertex();
         buffer.pos(blockX+0.5f,blockY+0.5f,blockZ+0.5f).color(gr,gg,gb,ga).endVertex();
+    }
+
+
+    private static void drawTexturedOutline(BufferBuilder buffer, float mx, float my, float mz, float r, float g, float b, float a, TextureAtlasSprite texture )
+    {
+        GlStateManager.enableTexture2D();
+        GlStateManager.enableAlpha();
+        GlStateManager.enableBlend();
+        float u0=texture.getInterpolatedU(0);
+        float u1=texture.getInterpolatedU(1);
+        float v0=texture.getInterpolatedV(0);
+        float v1=texture.getInterpolatedV(1);
+
+        // Face 1
+        buffer.pos(mx, my, mz).tex(u0,v0).endVertex(); //.color(r,g,b,a);
+        buffer.pos(mx,my+1,mz).tex(u1,v0).endVertex(); //.color(r,g,b,a).endVertex();
+        buffer.pos(mx+1,my,mz).tex(u0,v1).endVertex(); //.color(r,g,b,a).endVertex();
+        buffer.pos(mx, my+1, mz).tex(u1,v0).endVertex(); //.color(r,g,b,a).endVertex();
+        buffer.pos(mx+1,my+1,mz).tex(u1,v1).endVertex(); //.color(r,g,b,a).endVertex();
+        buffer.pos(mx+1,my,mz).tex(u0,v1).endVertex(); //.color(r,g,b,a).endVertex();
+
+
+        GlStateManager.disableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.disableTexture2D();
+
     }
 
     /**
