9748abc2b530bd92f6e29908ffcb01a15e0694b1
Katrina Swales <kat.swales@nekokittygames.com>
2020-04-30 00:20:37 +0100

    Updated mcmod.info Also added new enchanter layout to JEI restrictions

--- src/main/java/com/nekokittygames/thaumictinkerer/common/compat/jei/JEIEnchanterHandler.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/common/compat/jei/JEIEnchanterHandler.java
@@ -2,6 +2,7 @@ package com.nekokittygames.thaumictinkerer.common.compat.jei;
 
 import com.nekokittygames.thaumictinkerer.client.gui.GuiEnchanter;
 import mezz.jei.api.gui.IAdvancedGuiHandler;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.awt.*;
@@ -10,7 +11,7 @@ import java.util.List;
 
 public class JEIEnchanterHandler implements IAdvancedGuiHandler<GuiEnchanter> {
     @Override
-    public Class<GuiEnchanter> getGuiContainerClass() {
+    public @NotNull Class<GuiEnchanter> getGuiContainerClass() {
         return GuiEnchanter.class;
     }
 
@@ -19,15 +20,24 @@ public class JEIEnchanterHandler implements IAdvancedGuiHandler<GuiEnchanter> {
     public List<Rectangle> getGuiExtraAreas(GuiEnchanter guiContainer) {
         List<Rectangle> exclusions = new ArrayList<>();
         exclusions.add(new Rectangle(guiContainer.getX() + 176, guiContainer.getY() + 6, guiContainer.getVisRequireWidth(), guiContainer.getVisRequireHeight() + (guiContainer.getEnchanter().getEnchantmentCost().size() > 0 ? 18 : 0)));
+        int xOffset=0;
+        int y=0;
         for (int i = 0; i < guiContainer.getEnchanter().getEnchantments().size(); i++) {
-            exclusions.add(new Rectangle(guiContainer.getX() + 176, guiContainer.getY() + (i * 26) + 30 + 15, 34, 25));
+
+            if((guiContainer.getY() + (y * 26) + 30 + 15) +24 >guiContainer.height)
+            {
+                xOffset+=26+14+1;
+                y=0;
+            }
+            exclusions.add(new Rectangle(guiContainer.getX() + 176+xOffset, guiContainer.getY() + (y * 26) + 30 + 15, 34, 25));
+            y++;
         }
         return exclusions;
     }
 
     @Nullable
     @Override
-    public Object getIngredientUnderMouse(GuiEnchanter guiContainer, int mouseX, int mouseY) {
+    public Object getIngredientUnderMouse(@NotNull GuiEnchanter guiContainer, int mouseX, int mouseY) {
         return null;
     }
 }
