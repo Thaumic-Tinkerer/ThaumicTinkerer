e27ddbd4f4105aca5a186745e0ab909eba633eca
Katrina Swales <kat.swales@nekokittygames.com>
2020-06-03 01:42:14 +0100

    Removed accidental addition

--- src/main/java/com/nekokittygames/thaumictinkerer/common/misc/MiscHelper.java
+++ src/main/java/com/nekokittygames/thaumictinkerer/common/misc/MiscHelper.java
@@ -1,26 +1,12 @@
 package com.nekokittygames.thaumictinkerer.common.misc;
 
-import com.nekokittygames.thaumictinkerer.ThaumicTinkerer;
-import com.nekokittygames.thaumictinkerer.common.libs.LibMisc;
-import net.minecraft.client.audio.MovingSoundMinecartRiding;
 import net.minecraft.entity.Entity;
-import net.minecraftforge.client.event.sound.PlaySoundEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import thaumcraft.codechicken.lib.vec.Vector3;
 
-@Mod.EventBusSubscriber(modid = LibMisc.MOD_ID)
+
 public class MiscHelper {
 
-    @SubscribeEvent
-    public static void registerItems(PlaySoundEvent event)
-    {
 
-        if(event.getSound() instanceof MovingSoundMinecartRiding) {
-            MovingSoundMinecartRiding sound = (MovingSoundMinecartRiding) event.getSound();
-            ThaumicTinkerer.logger.info("Sound " + event.getSound().toString() + " Playing  at "+sound.getYPosF());
-        }
-    }
     public static void setEntityMotionFromVector(Entity entity, Vector3 originalPosVector, float modifier) {
         Vector3 entityVector = Vector3.fromEntityCenter(entity);
         Vector3 finalVector = originalPosVector.copy().subtract(entityVector);
