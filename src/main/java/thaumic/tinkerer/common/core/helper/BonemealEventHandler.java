fc8120d4fb9f417bbfc3dcc5c0295cba47214aed
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-02-12 14:54:23 -0600

    remove infused seed hoe of growth

--- src/main/java/thaumic/tinkerer/common/core/helper/BonemealEventHandler.java
+++ src/main/java/thaumic/tinkerer/common/core/helper/BonemealEventHandler.java
@@ -1,15 +1,8 @@
 package thaumic.tinkerer.common.core.helper;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.block.IGrowable;
 import net.minecraftforge.event.entity.player.BonemealEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import thaumcraft.common.Thaumcraft;
-import thaumcraft.common.blocks.BlockCustomPlant;
-import thaumcraft.common.config.ConfigItems;
-import thaumcraft.common.items.equipment.ItemElementalHoe;
 import thaumic.tinkerer.common.block.BlockInfusedGrain;
-import thaumic.tinkerer.common.block.tile.TileInfusedGrain;
 import thaumic.tinkerer.common.core.handler.ConfigHandler;
 
 /**
@@ -18,37 +11,12 @@ import thaumic.tinkerer.common.core.handler.ConfigHandler;
  * Prevents bonemeal being used on TT crops
  */
 public class BonemealEventHandler {
-
-    @SubscribeEvent
-    public void onBonemeal(BonemealEvent event) {
-        if (event.world.getBlock(event.x, event.y, event.z) instanceof BlockInfusedGrain) {
-            if (!ConfigHandler.cropsAllowBonemeal) {
-                event.setCanceled(true);
-            }
-        }
-    }
-    @SubscribeEvent
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (!event.world.isRemote) {
-            if (event.world.getBlock(event.x, event.y, event.z) instanceof BlockInfusedGrain) {
-                if (event.entityPlayer != null && event.entityPlayer.getCurrentEquippedItem() != null) {
-                    if (event.entityPlayer.getCurrentEquippedItem().getItem() == ConfigItems.itemHoeElemental) {
-                        IGrowable igrowable = (IGrowable) event.world.getBlock(event.x, event.y, event.z);
-
-                        if (igrowable.func_149851_a(event.world, event.x, event.y, event.z, event.world.isRemote)) {
-                            if (!event.world.isRemote) {
-                                if (igrowable.func_149852_a(event.world, event.world.rand, event.x, event.y, event.z)) {
-                                    igrowable.func_149853_b(event.world, event.world.rand, event.x, event.y, event.z);
-                                }
-                            }
-
-                        }
-                        event.entityPlayer.getCurrentEquippedItem().damageItem(25, event.entityPlayer);
-                        Thaumcraft.proxy.blockSparkle(event.world, event.x, event.y, event.z, 0, 2);
-                    }
-                }
-            }
-        }
-    }
-
+	
+	@SubscribeEvent
+	public void onBonemeal(BonemealEvent event) {
+		if ((event.world.getBlock(event.x, event.y, event.z) instanceof BlockInfusedGrain)
+				&& !ConfigHandler.cropsAllowBonemeal) {
+			event.setCanceled(true);
+		}
+	}
 }
